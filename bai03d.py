7ff213d19b02ba24bb149795530a48e829840999
Trần Thảo Hưng <124949488+thaohung13@users.noreply.github.com>
2023-04-15 12:56:27 +0700

    Update bai03d.py

--- bai03d.py
+++ bai03d.py
@@ -9,13 +9,23 @@ def detectAndDisplay(frame):
     faces = face_cascade.detectMultiScale(frame_gray)
     for (x,y,w,h) in faces:
         center = (x + w//2, y + h//2)
-        frame = cv2.ellipse(frame, center, (w//2, h//2), 0, 0, 360, (255, 0, 255), 4)   
+        frame = cv2.ellipse(frame, center, (w//2, h//2), 0, 0, 360, (255, 0, 255), 4)
+
+        faceROI = frame_gray[y:y+h,x:x+w]
+        #-- In each face, detect eyes
+        eyes = eyes_cascade.detectMultiScale(faceROI)
+        for (x2,y2,w2,h2) in eyes:
+            eye_center = (x + x2 + w2//2, y + y2 + h2//2)
+            radius = int(round((w2 + h2)*0.25))
+            frame = cv2.circle(frame, eye_center, radius, (255, 0, 0 ), 4)
     FRAME_WINDOW.image(frame)
 
 st.title("Webcam Live Feed")
 FRAME_WINDOW = st.image([])
 face_cascade = cv2.CascadeClassifier()
+eyes_cascade = cv2.CascadeClassifier()
 face_cascade.load('haarcascade_frontalface_alt.xml')
+eyes_cascade.load('haarcascade_eye_tree_eyeglasses.xml')
 cap = cv2.VideoCapture(0)
 
 while True:
